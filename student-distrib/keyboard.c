9466b38ac8cb620113ca0bf1a44ae7bdbf49a746
sun <ysunxh@gmail.com>
2017-03-12 20:18:24 -0500

    keyboard general

--- student-distrib/keyboard.c
+++ student-distrib/keyboard.c
@@ -5,6 +5,36 @@
 
 #include "keyboard.h"
 
+unsigned char scancode_set[128] =
+{
+    '\0',  0x1B, '1', '2', '3', '4', '5', '6', '7', '8',
+    '9', '0', '-', '=',
+    0x08, /* backspace */
+    0x09, /* tab */
+    'q', 'w', 'e', 'r','t', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n',/* enter */
+    0xA2,	/* left Control */
+    'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l',
+    ';','\'', '`',
+    0xA0,/* left shift */
+    '\\', 'z', 'x', 'c', 'v', 'b', 'n',
+    'm', ',', '.', '/',
+    0xA1,/* right shift */
+    '*',
+    0x12,	/* alt */
+    ' ',	/* space */
+    0x14,	/* caps lock */
+    '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0',/* F1 ... F10 */
+    '\0',	/* num lock */
+    '\0',	/* scroll Lock */
+    '\0', '\0', '\0',	/* keypad 7 8 9 */
+    '-',
+    '\0', '\0', '\0',	/* keypad 4 5 9 */
+    '+',
+    '\0', '\0', '\0', '\0', '\0', /* keypad 1 2 3 0 . */
+    '\0', '\0', '\0',	/* not used keys */
+    '\0', '\0',	/* F11 F12 Key */
+    '\0',	/* undefined keys*/
+};
 
 void
 keyboard_init(void){
@@ -31,10 +61,8 @@ keyboard_interrupt(void){
         if (scancode > 0) { break; }
     }
 
-    if( scancode == 0x24) { printf("J pressed \n"); }
-    // printk("Scan Code %x %s.\n",
-    //        (int) *((char *) scancode) & 0x7F,
-    //        *((char *) scancode) & 0x80 ? "Released" : "Pressed");
+    if( scancode < 0x80) { printf("%c pressed \n",scancode_set[scancode]); }
+    //        only print when it is pressed instead of released
 
     send_eoi(KEYBOARD_IRQ);
 
