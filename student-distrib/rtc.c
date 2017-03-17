7e8da856d67165897814af1c04daa1a88f16c80d
Wang <qwang65@illinois.edu>
2017-03-16 20:01:00 -0500

    rtc with comments

--- student-distrib/rtc.c
+++ student-distrib/rtc.c
@@ -19,8 +19,6 @@
  */
 
 
-
-
 void
 rtc_init(void){
     unsigned char prev;
@@ -48,12 +46,20 @@ rtc_init(void){
     sti();
 }
 
+/*
+rtc_open
+Description: Open the rtc driver and set the frequency to default value
+Input: None
+Output: None
+Return value: 0 - success
+Side Effect: The rtc is open
 
+*/
 int
 rtc_open(void)
 {
 
-    unsigned char rate = OPEN_FLAG;
+    unsigned char rate = OPEN_FLAG; //set the rate to be default value
     cli();
     outb(STATUS_REGISTER_A,NMI_PORT);
     char prev = inb(CMOS_PORT);
@@ -63,6 +69,16 @@ rtc_open(void)
     return 0;
 }
 
+/*
+rtc_close
+Description: Close the rtc driver
+Input: None
+Output: None
+Return value: 0 - success
+Side Effect: The rtc is closed
+
+*/
+
 int
 rtc_close(void)
 {
@@ -71,42 +87,62 @@ return 0;
 
 }
 
+/*
+rtc_read
+Description: Wait until last rtc interrupt ends
+Input: None
+Output: None
+Return value: 0 - success
+Side Effect: Wait until last rtc interrupt ends
+*/
 
 int rtc_read()
 {
+    cli();
     rtc_service = 1;
+    sti();
     while (rtc_service)
     {}
     return 0;
 }
 
 
+/*
+rtc_write
+Description: Set the frequency.
+Input: frequency - the frequency to be set
+Output: None
+Return value: 0 - success
+             -1 - failure
+Side Effect: The rtc's frequency is set
+
+*/
 
 int
 rtc_write(unsigned int frequency)
 {
     unsigned char rate = 0;
-    if (frequency > HIGHEST_BIT_MASK)
+    if (frequency > HIGHEST_BIT_MASK)   //check if the frequency exceed the max value
     {
         //printf("Invalid frequency\n");
         return -1;
     }
 
-    if (frequency == 0)
+    if (frequency == 0)                 //if f = 0, rate = 0
         rate = 0;
     else
     {
-        if ((frequency & (frequency - 1)) != 0)
+        if ((frequency & (frequency - 1)) != 0) //check if input frequency is power of 2 
         {
                 //printf("Invalid frequency\n");
-                return -1;
+                return -1;                      //if not, return -1
         }
 
         rate = MIN_FREQUENCY;
         while ((frequency & HIGHEST_BIT_MASK) == 0)
             {
                 rate++;
-               frequency = frequency<<1;
+               frequency = frequency<<1;        //find the rate
             }
 
     }
